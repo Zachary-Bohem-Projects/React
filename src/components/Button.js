4698b02fe75b3c5baa0fc8877af951beb2615fd3
Zachary-Boehm <60978390+Zachary-Boehm@users.noreply.github.com>
2021-08-31 10:10:40 -0500

    .

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,25 @@
 import PropTypes from 'prop-types'
 
-function Button({ color , text, onClick}) {
-    return (
-        <button onClick={onClick} style ={{backgroundColor: color}} className='btn'>{text}</button>
-    )
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
+      {text}
+    </button>
+  )
 }
 
 Button.defaultProps = {
-    color: 'black',
-    text: 'Add'
+  color: 'steelblue',
 }
 
 Button.propTypes = {
-    color: PropTypes.string,
-    text: PropTypes.string,
-    onClick: PropTypes.func
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,36 @@
 import PropTypes from 'prop-types'
-import Button from './Button'
 import { useLocation } from 'react-router-dom'
+import Button from './Button'
 
-const Header = ({title, onAdd, showAdd}) => {
-    const location = useLocation()
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            {location.pathname === '/' && <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>}
-        </header>
-    )
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker',
+  title: 'Task Tracker',
 }
 
 Header.propTypes = {
-    title: PropTypes.string,
+  title: PropTypes.string.isRequired,
 }
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
+// }
+
 export default Header
